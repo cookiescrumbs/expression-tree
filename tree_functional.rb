01f8dbe67d7f141baf37a0defd2c0070530c740b
Steven Cook <stevenjamescook@gmail.com>
2022-02-20 17:05:51 +0000

    with closures ... lambdas

--- tree_functional.rb
+++ tree_functional.rb
@@ -1,37 +1,15 @@
-def add(left, right)
-  left + right 
-end
+add = -> (left, right) { left + right }
+sub = -> (left, right) { left - right }
+mul = -> (left, right) { left * right }
+div = -> (left, right) { left / right }
 
-def sub(left, right)
-  left - right
-end
-
-def mul(left, right)
-  left * right
-end
-
-def div(left, right)
-  left / right
-end
+add_s = -> (left, right) { "(#{left} + #{right})" }
+sub_s = -> (left, right) { "(#{left} - #{right})" }
+mul_s = -> (left, right) { "(#{left} x #{right})" }
+div_s= -> (left, right)  { "(#{left} ÷ #{right})" }
 
 def num(value) 
   value
-end
-
-def add_s(left, right)
-  "(#{left} + #{right})"
-end
-
-def sub_s(left, right)
-  "(#{left} - #{right})"
-end
-
-def mul_s(left, right)
-  "(#{left} x #{right})"
-end
-
-def div_s(left, right)
-  "(#{left} ÷ #{right})"
 end
 
 
@@ -44,5 +22,5 @@ def assert_equal(expected, actual)
   end
 end
 
-assert_equal "((7 + ((3 - 2) x 5)) ÷ 6)", div_s(add_s(num(7),mul_s(sub_s(num(3),num(2)),num(5))),num(6))
-assert_equal 2, div(add(num(7),mul(sub(num(3),num(2)),num(5))),num(6))
+assert_equal "((7 + ((3 - 2) x 5)) ÷ 6)", div_s.call(add_s.call(num(7),mul_s.call(sub_s.call(num(3),num(2)),num(5))),num(6))
+assert_equal 2, div.call(add.call(num(7),mul.call(sub.call(num(3),num(2)),num(5))),num(6))
