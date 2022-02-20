941445648a5e0c7af9c0336039b15c720ef8d1d0
Steven Cook <stevenjamescook@gmail.com>
2022-02-20 18:40:58 +0000

    used ternary operator and closure to capture print state

--- tree_functional.rb
+++ tree_functional.rb
@@ -1,15 +1,12 @@
-add = -> (left, right) { left + right }
-sub = -> (left, right) { left - right }
-mul = -> (left, right) { left * right }
-div = -> (left, right) { left / right }
 
-add_s = -> (left, right) { "(#{left} + #{right})" }
-sub_s = -> (left, right) { "(#{left} - #{right})" }
-mul_s = -> (left, right) { "(#{left} x #{right})" }
-div_s= -> (left, right)  { "(#{left} ÷ #{right})" }
-
-def num(value) 
-  value
+def tree(print: false)
+  add = -> (left, right) { print ? "(#{left} + #{right})" : left + right }
+  sub = -> (left, right) { print ? "(#{left} - #{right})" : left - right }
+  mul = -> (left, right) { print ? "(#{left} x #{right})" : left * right }
+  div = -> (left, right) { print ? "(#{left} ÷ #{right})" : left / right }
+  num = -> (value) { value }
+  
+  div.(add.(num.(7),mul.(sub.(num.(3),num.(2)),num.(5))),num.(6))
 end
 
 
@@ -22,5 +19,5 @@ def assert_equal(expected, actual)
   end
 end
 
-assert_equal "((7 + ((3 - 2) x 5)) ÷ 6)", div_s.call(add_s.call(num(7),mul_s.call(sub_s.call(num(3),num(2)),num(5))),num(6))
-assert_equal 2, div.call(add.call(num(7),mul.call(sub.call(num(3),num(2)),num(5))),num(6))
+assert_equal "((7 + ((3 - 2) x 5)) ÷ 6)", tree(print: true)
+assert_equal 2, tree
